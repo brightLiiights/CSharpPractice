bd0c38246a6e9fc24d2040ea0090b300becb0ec0
alex <alejandro@alejandrosanchez.tech>
2022-03-20 15:32:41 -0500

    First Commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+namespace CSharpPractice
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World! :)");
+        }
+    }
+}
